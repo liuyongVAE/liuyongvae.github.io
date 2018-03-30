e4adb71b8edce5ed132c0ea20ec37cf9b89d8777
liuyongVAE <liuyong_v@163.com>
2018-03-30 16:20:55 +0800

    Update myjs.js

--- js/myjs.js
+++ js/myjs.js
@@ -1,31 +1,31 @@
 
 
 $(document).ready(function(){
-	$(".return-div").hide();
-	$(".return-btn").hide();
-	$("li").click( function(){
+// 	$(".return-div").hide();
+// 	$(".return-btn").hide();
+// 	$("li").click( function(){
 		
-	 $("header").animate({left:"-1050px"},"slow")	
-	 $(".panel-main").animate({left:"1000",width:"320px"},"slow")
-	 $("#mainp").hide();
-	 $(".line-2").hide();
-	 $(".aboutlist").hide();
-	 $(".return-div").show();
-	 $(".return-btn").show();
+// 	 $("header").animate({left:"-1050px"},"slow")	
+// 	 $(".panel-main").animate({left:"1000",width:"320px"},"slow")
+// 	 $("#mainp").hide();
+// 	 $(".line-2").hide();
+// 	 $(".aboutlist").hide();
+// 	 $(".return-div").show();
+// 	 $(".return-btn").show();
 	 
 	 
 	 
      });
      
      
-	 $(".return-div").click(function(){
-	 	 console.log("hello");
-	       $("header").animate({left:"0px"},"slow")
-	   	   $(".panel-main").animate({left:"0px",width:"100%"},"slow")
-	       $("#mainp").show();
-	       $(".line-2").show();
-	       $(".aboutlist").show();
-     });
+// 	 $(".return-div").click(function(){
+// 	 	 console.log("hello");
+// 	       $("header").animate({left:"0px"},"slow")
+// 	   	   $(".panel-main").animate({left:"0px",width:"100%"},"slow")
+// 	       $("#mainp").show();
+// 	       $(".line-2").show();
+// 	       $(".aboutlist").show();
+//      });
       $(".return-btn").click(function(){
 	 	 console.log("hello");
 	       $("header").animate({left:"0px"},"slow")
